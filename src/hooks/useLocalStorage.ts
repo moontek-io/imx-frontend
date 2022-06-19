5b4b6552978b12946448bd6ebed94fb65dcf2a59
Mudit Jain <muditjdev@gmail.com>
2022-06-19 16:38:23 +0530

    fix uselocalstorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export default function useLocalStorage<T>(key: string, initialValue: T) {
   // State to store our value
@@ -20,7 +20,7 @@ export default function useLocalStorage<T>(key: string, initialValue: T) {
   });
   // Return a wrapped version of useState's setter function that ...
   // ... persists the new value to localStorage.
-  const setValue = (value: T | ((val: T) => T)) => {
+  const setValue = useCallback((value: T | ((val: T) => T)) => {
     try {
       // Allow value to be a function so we have same API as useState
       const valueToStore =
@@ -35,6 +35,6 @@ export default function useLocalStorage<T>(key: string, initialValue: T) {
       // A more advanced implementation would handle the error case
       console.log(error);
     }
-  };
+  }, [key, storedValue]);
   return [storedValue, setValue] as const;
 }
